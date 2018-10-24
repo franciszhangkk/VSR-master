8caf7a16881a3a9252ecf0d263902ff1011a6f55
franciszhangkk <axczd12@163.com>
2018-10-25 10:09:55 +1100

    combine

--- trainer/new_task.py
+++ trainer/new_task.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 from model import FRVSR_model
-=======
 from model import  FRVSR_model_2
->>>>>>> 91784677090d23f0e5e080c535bc3c042e281a4a
 from keras.optimizers import Adam
 import imageio
 import numpy as np
@@ -16,7 +13,7 @@ from keras.callbacks import TensorBoard
 output_size = (512, 512)
 input_size = (256, 256)
 
-batch_size = 4
+batch_size = 2
 
 
 def read_file_names():
@@ -57,8 +54,43 @@ def generator1(filenames):
         result, l = [np.asarray(batch_list), low_res_black, high_res_black], np.asarray(label_list)
         yield result, l
 
+def generator(filenames):
+    batch_list1 = [None] * batch_size
+    label_list1 = [None] * batch_size
+    batch_list2 = [None] * batch_size
+    label_list2 = [None] * batch_size
+    low_res_black = np.zeros((batch_size, input_size[1], input_size[0], 3))
+    high_res_black = np.zeros((batch_size, output_size[1], output_size[0], 3))
+    while True:
+        for i in range(batch_size):
+            index = random.choice(range(len(filenames)))
+            image_names = read_image_names(filenames[index])
+            low_res_list = []
+            high_res_list = []
+            for j in range(0, len(image_names)):
+                # print(image_names,'im len')
+                high_res, low_res = read_image(image_names[j])
+                low_res_list.append(low_res)
+                high_res_list.append(high_res)
+            batch_list1[i] = low_res_list[0]
+            label_list1[i] = high_res_list[0]
+            batch_list2[i] = low_res_list[1]
+            label_list2[i] = high_res_list[1]
+
+        result, l = [np.asarray(batch_list1), np.asarray(batch_list2), low_res_black, high_res_black], [np.asarray(label_list1),np.asarray(label_list1)]
+        yield result, l
+
 # def generator(filenames):
 #     batch_list1 = [None] * batch_size
+#     batch_list2 = [None] * batch_size
+#     batch_list3 = [None] * batch_size
+#     batch_list4 = [None] * batch_size
+#     batch_list5 = [None] * batch_size
+#     label_list1 = [None] * batch_size
+#     label_list2 = [None] * batch_size
+#     label_list3 = [None] * batch_size
+#     label_list4 = [None] * batch_size
+#     label_list5 = [None] * batch_size
 #     label_list1 = [None] * batch_size
 #     batch_list2 = [None] * batch_size
 #     label_list2 = [None] * batch_size
@@ -79,60 +111,17 @@ def generator1(filenames):
 #             label_list1[i] = high_res_list[0]
 #             batch_list2[i] = low_res_list[1]
 #             label_list2[i] = high_res_list[1]
+#             batch_list3[i] = low_res_list[2]
+#             label_list3[i] = high_res_list[2]
+#             batch_list4[i] = low_res_list[3]
+#             label_list4[i] = high_res_list[3]
+#             batch_list5[i] = low_res_list[4]
+#             label_list5[i] = high_res_list[4]
 #
+#         result, l = [np.asarray(batch_list1), np.asarray(batch_list2), np.asarray(batch_list3), np.asarray(batch_list4),
+#                      np.asarray(batch_list5), low_res_black, high_res_black], [np.asarray(label_list1), np.asarray(label_list2), np.asarray(label_list3), np.asarray(label_list4), np.asarray(label_list5)]
 #         result, l = [np.asarray(batch_list1), np.asarray(batch_list2), low_res_black, high_res_black], [np.asarray(label_list1),np.asarray(label_list1)]
 #         yield result, l
-
-def generator(filenames):
-    batch_list1 = [None] * batch_size
-<<<<<<< HEAD
-    batch_list2 = [None] * batch_size
-    batch_list3 = [None] * batch_size
-    batch_list4 = [None] * batch_size
-    batch_list5 = [None] * batch_size
-    label_list1 = [None] * batch_size
-    label_list2 = [None] * batch_size
-    label_list3 = [None] * batch_size
-    label_list4 = [None] * batch_size
-    label_list5 = [None] * batch_size
-
-=======
-    label_list1 = [None] * batch_size
-    batch_list2 = [None] * batch_size
-    label_list2 = [None] * batch_size
->>>>>>> 91784677090d23f0e5e080c535bc3c042e281a4a
-    low_res_black = np.zeros((batch_size, input_size[1], input_size[0], 3))
-    high_res_black = np.zeros((batch_size, output_size[1], output_size[0], 3))
-    while True:
-        for i in range(batch_size):
-            index = random.choice(range(len(filenames)))
-            image_names = read_image_names(filenames[index])
-            low_res_list = []
-            high_res_list = []
-            for j in range(0, len(image_names)):
-                # print(image_names,'im len')
-                high_res, low_res = read_image(image_names[j])
-                low_res_list.append(low_res)
-                high_res_list.append(high_res)
-            batch_list1[i] = low_res_list[0]
-            label_list1[i] = high_res_list[0]
-            batch_list2[i] = low_res_list[1]
-            label_list2[i] = high_res_list[1]
-<<<<<<< HEAD
-            batch_list3[i] = low_res_list[2]
-            label_list3[i] = high_res_list[2]
-            batch_list4[i] = low_res_list[3]
-            label_list4[i] = high_res_list[3]
-            batch_list5[i] = low_res_list[4]
-            label_list5[i] = high_res_list[4]
-
-        result, l = [np.asarray(batch_list1), np.asarray(batch_list2), np.asarray(batch_list3), np.asarray(batch_list4),
-                     np.asarray(batch_list5), low_res_black, high_res_black], [np.asarray(label_list1), np.asarray(label_list2), np.asarray(label_list3), np.asarray(label_list4), np.asarray(label_list5)]
-=======
-
-        result, l = [np.asarray(batch_list1), np.asarray(batch_list2), low_res_black, high_res_black], [np.asarray(label_list1),np.asarray(label_list1)]
->>>>>>> 91784677090d23f0e5e080c535bc3c042e281a4a
-        yield result, l
 
 
 if __name__ == '__main__':
@@ -148,7 +137,7 @@ if __name__ == '__main__':
     superes_net.model.compile(
         optimizer=Adam(lr=adam_lr),
         loss='mean_squared_error',
-        loss_weights=[1., 1., 1., 1., 1.])
+        loss_weights=[1., 1.])
 
     # low_res_input = np.asarray(low_res)
     # high_res_input = np.asarray(high_res)
