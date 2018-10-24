3314deda2b4badcf511e511eae93e93a3345f18e
franciszhangkk <axczd12@163.com>
2018-10-25 10:00:19 +1100

    train

--- trainer/new_task.py
+++ trainer/new_task.py
@@ -1,4 +1,4 @@
-from model import  FRVSR_model
+from model import FRVSR_model
 from keras.optimizers import Adam
 import imageio
 import numpy as np
@@ -13,6 +13,7 @@ output_size = (512, 512)
 input_size = (256, 256)
 
 batch_size = 4
+
 
 def read_file_names():
     file_names = []
@@ -31,10 +32,12 @@ def read_image_names(file):
     print("Loaded ", len(image_names))
     return image_names
 
+
 def read_image(filename):
     high_res = imageio.imread(filename).astype(float)
     low_res = scipy.misc.imresize(high_res, size=50, interp='bilinear')
     return high_res, low_res
+
 
 def generator1(filenames):
     batch_list = [None] * batch_size
@@ -52,8 +55,17 @@ def generator1(filenames):
 
 
 def generator(filenames):
-    batch_list = [None] * batch_size
-    label_list = [None] * batch_size
+    batch_list1 = [None] * batch_size
+    batch_list2 = [None] * batch_size
+    batch_list3 = [None] * batch_size
+    batch_list4 = [None] * batch_size
+    batch_list5 = [None] * batch_size
+    label_list1 = [None] * batch_size
+    label_list2 = [None] * batch_size
+    label_list3 = [None] * batch_size
+    label_list4 = [None] * batch_size
+    label_list5 = [None] * batch_size
+
     low_res_black = np.zeros((batch_size, input_size[1], input_size[0], 3))
     high_res_black = np.zeros((batch_size, output_size[1], output_size[0], 3))
     while True:
@@ -64,13 +76,23 @@ def generator(filenames):
             high_res_list = []
             for j in range(0, len(image_names)):
                 high_res, low_res = read_image(image_names[j])
-                low_res_list.append(low_res_list)
-                high_res_list.append(high_res_list)
-            batch_list[i] = low_res_list
-            label_list[i] = high_res_list
+                low_res_list.append(low_res)
+                high_res_list.append(high_res)
+            batch_list1[i] = low_res_list[0]
+            label_list1[i] = high_res_list[0]
+            batch_list2[i] = low_res_list[1]
+            label_list2[i] = high_res_list[1]
+            batch_list3[i] = low_res_list[2]
+            label_list3[i] = high_res_list[2]
+            batch_list4[i] = low_res_list[3]
+            label_list4[i] = high_res_list[3]
+            batch_list5[i] = low_res_list[4]
+            label_list5[i] = high_res_list[4]
 
-        result, l = [np.asarray(batch_list), low_res_black, high_res_black], np.asarray(label_list)
+        result, l = [np.asarray(batch_list1), np.asarray(batch_list2), np.asarray(batch_list3), np.asarray(batch_list4),
+                     np.asarray(batch_list5), low_res_black, high_res_black], [np.asarray(label_list1), np.asarray(label_list2), np.asarray(label_list3), np.asarray(label_list4), np.asarray(label_list5)]
         yield result, l
+
 
 if __name__ == '__main__':
     KTF.set_session(tf.Session(config=tf.ConfigProto(device_count={'gpu': 0})))
@@ -78,23 +100,22 @@ if __name__ == '__main__':
 
     tensorboard = TensorBoard(log_dir="./logs", write_graph=True, write_images=True)
 
-    #low_res, high_res = read_input()
+    # low_res, high_res = read_input()
     filenames = read_file_names()
     superes_net = FRVSR_model((input_size[1], input_size[0], 3), (output_size[1], output_size[0], 3))
     # create the FRVSR object
     superes_net.model.compile(
         optimizer=Adam(lr=adam_lr),
-        loss='mean_squared_error'
-    )
+        loss='mean_squared_error',
+        loss_weights=[1., 1., 1., 1., 1.])
 
     # low_res_input = np.asarray(low_res)
     # high_res_input = np.asarray(high_res)
     # low_res_black = np.zeros(low_res_input.shape)
     # high_res_black = np.zeros(high_res_input.shape)
 
-    #superes_net.model.fit([low_res_input, low_res_black, high_res_black], high_res_input, epochs=10, callbacks=[tensorboard], validation_split=0.2)
+    # superes_net.model.fit([low_res_input, low_res_black, high_res_black], high_res_input, epochs=10, callbacks=[tensorboard], validation_split=0.2)
     superes_net.model.fit_generator(generator(filenames), epochs=10, samples_per_epoch=32, callbacks=[tensorboard])
-
 
     superes_net.model.save("model.h5")
 
